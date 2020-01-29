46aa58026a1c710d13cd1f8f5c0fa340d847a007
fridaperez <falejandraperez@g.ucla.edu>
2020-01-29 11:27:10 -0800

    updated extractbodym script

--- ExtractBodyM.sh
+++ ExtractBodyM.sh
@@ -1,71 +1,12 @@
 #the command used in the Bodym.csv file was: 
 
-#1 beginning from the second line 
-tail -n +2 Pacifici2013_data.csv
-
-#2 choosing the second to sixth column using the cut command 
-cut -d ";" -f 2-6
-
-#3 changing the current delimeter to spaces with the tr command 
-tr ";" "" 
-
-#4 sorting  the selected columns in a numerical order -n in reverse order -r and according to the sixth  column  -k
-sort -r -n -k 6 
+#beginning from the second line 
+#choosing the second to sixth column using the cut command 
+#changing the current delimeter to spaces with the tr command  
+#sorting  the selected columns in a numerical order -n in reverse order -r and according to the sixth  column  -k 
 
 #All the commands piped together would look like this 
-
-tail -n +2 Pacifici2013_data.csv| cut -d ";" -f 2-6| tr ";" ""| sort -r -n -k 6
+tail -n +2 ~/Developer/repos/CSB/unix/data/Pacifici2013_data.csv | cut -d ';' -f 2-6| tr -s ';' ' ' | sort -r -n -k 6 > BodyM.csv
 
 #In order to make the script  run automatically without having to type  bash command   you first have to make the script executable by changing the permission of the file with the chmod command and then adding the  shebang at the beginning of the script (#!/bin/bash)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
