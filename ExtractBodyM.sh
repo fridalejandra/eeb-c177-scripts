89017d05a6ed686d37894c6e6e47febff4f68a79
fridalejandra <falejandraperez@g.ucla.edu>
2020-02-26 10:38:23 -0800

    Updated ExtractFile

--- ExtractBodyM.sh
+++ ExtractBodyM.sh
@@ -1,9 +1,22 @@
+#!/bin/bash
+INPUT=$1
+DELIM=$2
+OUTPUT=$3
+
+tail -n +2 $INPUT|cut -d $DELIM -f 2-6|tr $DELIM " "|sort -r -n -k 6 >> $OUTPUT
+
+
+
+
+
+
 #the command used in the Bodym.csv file was: 
 
-#beginning from the second line 
+#beginning from the second line, skip the first line 
 #choosing the second to sixth column using the cut command 
 #changing the current delimeter to spaces with the tr command  
-#sorting  the selected columns in a numerical order -n in reverse order -r and according to the sixth  column  -k 
+#sorting  the selected columns in a numerical order -n in reverse order -r and according to the sixth  column  -k
+#In order to not type in bach command we can use the bin/bash to move the script into the usrbin  
 
 #All the commands piped together would look like this 
 tail -n +2 ~/Developer/repos/CSB/unix/data/Pacifici2013_data.csv | cut -d ';' -f 2-6| tr -s ';' ' ' | sort -r -n -k 6 > BodyM.csv
